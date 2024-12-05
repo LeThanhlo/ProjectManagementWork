d213a9bb2175cda99f844b20b90a552131deca9a
TuanNguyenGomi <longle@gomicorp.vn>
2024-12-05 12:22:20 +0700

    update 2024-12-5

--- Model/Users/Users.cs
+++ Model/Users/Users.cs
@@ -9,6 +9,9 @@ namespace Container_App.Model.Users
         public string Username { get; set; }
         public string Password { get; set; }
         public string FullName { get; set; }     
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
         public bool IsDel {  get; set; }
     }
 }
